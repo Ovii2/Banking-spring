684bb8572da5e847402a4cf4a2534cc5cd729708
Ovidijus <ovidijus.e@gmail.com>
2025-01-27 14:34:53 +0200

    Update navigation when user is logged in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import './index.css';
 import { Route, Routes } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import NotFoundPage from './pages/NotFoundPage';
-
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import HomePage from './pages/HomePage';
 import { ToastContainer } from 'react-toastify';
 import AccountPage from './pages/AccountPage';
+import UserContext from './context/UserContext';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { isLoggedIn } = useContext(UserContext);
 
   return (
     <div className='flex flex-col min-h-screen'>
@@ -20,10 +20,15 @@ function App() {
       <Header />
       <main className='flex-grow'>
         <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/account' element={<AccountPage />} />
+          {!isLoggedIn ? (
+            <>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/register' element={<RegisterPage />} />
+              <Route path='/login' element={<LoginPage />} />
+            </>
+          ) : (
+            <Route path='/account' element={<AccountPage />} />
+          )}
           <Route path='*' element={<NotFoundPage />} />
         </Routes>
       </main>
--- frontend/src/components/navigation/Navigation.jsx
+++ frontend/src/components/navigation/Navigation.jsx
@@ -13,7 +13,7 @@ import { Link, useLocation } from 'react-router-dom';
 import UserContext from '../../context/UserContext';
 
 const navigation = [
-  { name: 'Home', href: '/home' },
+  { name: 'Home', href: '/' },
   { name: 'Login', href: '/login' },
   { name: 'Register', href: '/register' },
   { name: 'Account', href: '/account' },
@@ -85,39 +85,39 @@ const Navigation = () => {
             </button>
 
             {/* Profile dropdown */}
-            <Menu as='div' className='relative ml-3'>
-              <div>
-                <MenuButton className='relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden'>
-                  <span className='absolute -inset-1.5' />
-                  <span className='sr-only'>Open user menu</span>
-                  <img
-                    alt=''
-                    src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                    className='size-8 rounded-full'
-                  />
-                </MenuButton>
-              </div>
-              <MenuItems
-                transition
-                className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in'
-              >
-                <MenuItem>
-                  <a
-                    href='#'
-                    className='block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden'
-                  >
-                    Your Profile
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href='#'
-                    className='block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden'
-                  >
-                    Settings
-                  </a>
-                </MenuItem>
-                {isLoggedIn && (
+            {isLoggedIn && (
+              <Menu as='div' className='relative ml-3'>
+                <div>
+                  <MenuButton className='relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden'>
+                    <span className='absolute -inset-1.5' />
+                    <span className='sr-only'>Open user menu</span>
+                    <img
+                      alt=''
+                      src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                      className='size-8 rounded-full'
+                    />
+                  </MenuButton>
+                </div>
+                <MenuItems
+                  transition
+                  className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in'
+                >
+                  <MenuItem>
+                    <a
+                      href='#'
+                      className='block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden'
+                    >
+                      Your Profile
+                    </a>
+                  </MenuItem>
+                  <MenuItem>
+                    <a
+                      href='#'
+                      className='block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden'
+                    >
+                      Settings
+                    </a>
+                  </MenuItem>
                   <MenuItem>
                     <button
                       onClick={logoutHandler}
@@ -126,9 +126,9 @@ const Navigation = () => {
                       Sign out
                     </button>
                   </MenuItem>
-                )}
-              </MenuItems>
-            </Menu>
+                </MenuItems>
+              </Menu>
+            )}
           </div>
         </div>
       </div>
